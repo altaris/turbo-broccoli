3313f74efad255912fc90011e580de58a8781062
Cédric HT <altaris@users.noreply.github.com>
2024-02-07 13:13:32 +0900

    Removed unnecessary method

--- turbo_broccoli/utils.py
+++ turbo_broccoli/utils.py
@@ -1,8 +1,5 @@
 """Various utilities and internal methods"""
 
-import re
-from typing import Any, Generator
-
 
 class DeserializationError(Exception):
     """Raised whenever something went wrong during deserialization"""
@@ -22,45 +19,3 @@ class TypeNotSupported(Exception):
 
 class TypeIsNodecode(Exception):
     """Raised during deserialization if the type shouldn't be decoded"""
-
-
-def artifacts(doc: Any) -> Generator[str, None, None]:
-    """
-    Lists all the artifacts names referenced by this JSON document. Obviously,
-    it should have been deserialized using vanilla `json.load` or `json.loads`,
-    or using turbo broccoli with adequate nodecodes.
-
-    In reality, this method recursively traverses the document and searches for
-    dicts that:
-
-    - have a `"__version__"`, `"__type__"`, and a `"id"` field;
-
-    - the value at `"id"` is a UUID4 or has the form `<uuid4>.<...>`, i.e.
-      matches the regexp
-
-        ```re
-        ^[0-9a-f]{8}(\\-[0-9a-f]{4}){3}\\-[0-9a-f]{12}(\\..+)?$
-        ```
-
-      in which case that value is `yield`ed.
-
-    TODO:
-        Implement a smarter way
-    """
-    if isinstance(doc, dict):
-        fields = ["__version__", "__type__", "id"]
-        if all(map(lambda f: f in doc, fields)):
-            v = doc["id"]
-            r = re.compile(
-                r"^[0-9a-f]{8}(\-[0-9a-f]{4}){3}\-[0-9a-f]{12}(\..+)?$"
-            )
-            if r.match(v):
-                yield v + ".tb"
-        else:
-            for v in doc.values():
-                for a in artifacts(v):
-                    yield a
-    elif isinstance(doc, list):
-        for x in doc:
-            for a in artifacts(x):
-                yield a
