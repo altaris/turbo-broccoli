0c547cc696654a55d296e2091d454f2f66a130a0
Cédric HT <altaris@users.noreply.github.com>
2024-02-07 17:02:30 +0900

    Bytes can produce artifacts

--- tests/test_bytes.py
+++ tests/test_bytes.py
@@ -3,6 +3,8 @@
 
 from common import assert_to_from_json
 
+from turbo_broccoli import Context, from_json, to_json
+
 
 def test_bytes_empty():
     assert_to_from_json(b"")
@@ -14,3 +16,12 @@ def test_bytes_ascii():
 
 def test_bytes_utf8():
     assert_to_from_json("Hello 👋".encode("utf8"))
+
+
+def test_bytes_large():
+    x = ("👋" * 1000).encode("utf8")
+    ctx = Context(min_artifact_size=0)
+    doc = to_json(x, ctx)
+    assert "id" in doc
+    assert "data" not in doc
+    assert x == from_json(doc, ctx)
--- turbo_broccoli/custom/bytes.py
+++ turbo_broccoli/custom/bytes.py
@@ -1,6 +1,7 @@
 """bytes (de)serialization utilities."""
 
 from base64 import b64decode, b64encode
+from math import ceil
 from typing import Any
 
 from turbo_broccoli.context import Context
@@ -11,11 +12,19 @@ def _bytes_from_json_v2(dct: dict, ctx: Context) -> bytes:
     return b64decode(dct["data"])
 
 
+def _bytes_from_json_v3(dct: dict, ctx: Context) -> bytes:
+    if "data" in dct:
+        return b64decode(dct["data"])
+    path = ctx.id_to_artifact_path(dct["id"])
+    with path.open(mode="rb") as fp:
+        return fp.read()
+
+
 # pylint: disable=missing-function-docstring
 def from_json(dct: dict, ctx: Context) -> bytes | None:
     DECODERS = {
-        # 1: _bytes_from_json_v2,  # Use turbo_broccoli v3
         2: _bytes_from_json_v2,
+        3: _bytes_from_json_v3,
     }
     try:
         return DECODERS[dct["__version__"]](dct, ctx)
@@ -25,20 +34,37 @@ def from_json(dct: dict, ctx: Context) -> bytes | None:
 
 def to_json(obj: Any, ctx: Context) -> dict:
     """
-    Serializes a Python `bytes` object into JSON using a base 64 + ASCII
+    Serializes a Python `bytes` object into JSON using a base64 + ASCII
     scheme. The return dict has the following structure
 
         {
             "__type__": "bytes",
-            "__version__": 2,
+            "__version__": 3,
             "data": <ASCII str>,
         }
 
+    or
+
+        {
+            "__type__": "bytes",
+            "__version__": 3,
+            "id": <uuid4>,
+        }
+
+    if the base64 encoding of the object is too large.
+
     """
-    if isinstance(obj, bytes):
+    if not isinstance(obj, bytes):
+        raise TypeNotSupported()
+    # https://stackoverflow.com/a/32140193
+    b64_size = (ceil((len(obj) * 4) / 3) + 3) & ~3
+    if b64_size <= ctx.min_artifact_size:
         return {
             "__type__": "bytes",
-            "__version__": 2,
+            "__version__": 3,
             "data": b64encode(obj).decode("ascii"),
         }
-    raise TypeNotSupported()
+    path, name = ctx.new_artifact_path()
+    with path.open(mode="wb") as fp:
+        fp.write(obj)
+    return {"__type__": "bytes", "__version__": 3, "id": name}
