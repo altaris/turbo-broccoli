08640f1e7f7fd59411bf52e5f41ab7107d9f801b
Cédric HT <altaris@users.noreply.github.com>
2024-04-02 11:52:34 +0900

    [fix] Don't import secret stuff if nacl not installed

--- turbo_broccoli/__init__.py
+++ turbo_broccoli/__init__.py
@@ -7,15 +7,20 @@ from pkg_resources import DistributionNotFound, get_distribution
 
 from turbo_broccoli.context import Context
 from turbo_broccoli.custom.embedded import EmbeddedDict, EmbeddedList
-from turbo_broccoli.custom.secret import (
-    LockedSecret,
-    Secret,
-    SecretDict,
-    SecretFloat,
-    SecretInt,
-    SecretList,
-    SecretStr,
-)
+
+try:
+    from turbo_broccoli.custom.secret import (
+        LockedSecret,
+        Secret,
+        SecretDict,
+        SecretFloat,
+        SecretInt,
+        SecretList,
+        SecretStr,
+    )
+except ModuleNotFoundError:
+    pass
+
 from turbo_broccoli.guard import GuardedBlockHandler
 from turbo_broccoli.native import load, save
 from turbo_broccoli.turbo_broccoli import (
